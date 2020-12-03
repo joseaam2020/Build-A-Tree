1133425c1499df72a538f604daedc942e5694c15
joseaam2020 <joseaam2019@hotmail.com>
2020-12-03 16:48:59 -0600

    Creacion de playerHub

---
+++ Assets/src/PlayerHub.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHub : MonoBehaviour
+{
+
+    [SerializeField] public GameObject[] modelList;
+
+    private void Awake()
+    {
+        foreach(GameObject model in modelList)
+        {
+            model.SetActive(false);
+        }
+    }
+}
